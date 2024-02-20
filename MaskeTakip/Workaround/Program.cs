3fdcd74b3968039cd9c4f8b0155c5e1e25fdd3da
Fatih Demirtaş <fatih.yzm20@gmail.com>
2024-02-20 17:02:01 +0300

    Proje Bitti

---
+++ MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person1 = new Person();
+        person1.FirstName = "FATİH";
+        person1.LastName = "DEMİRTAŞ";
+        person1.DateOfBirthYear = 2000;
+        person1.NationalIdentity = 12345;
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person1);
+
+        Console.ReadLine();
+    }
+}
